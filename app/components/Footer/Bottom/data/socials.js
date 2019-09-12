8d37e1ebc8323d429663c7f67720dcc3bdb47a27
OneGoodReas0n <korobovi4@gmail.com>
2019-09-12 03:41:32 +0300

    Implementing component Footer and subcomponents SiteMap and Bottom

---
+++ app/components/Footer/Bottom/data/socials.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import FacebookSVG from '../assets/facebook.svg';
+import InstagrammSVG from '../assets/instagramm.svg';
+import TwitterSVG from '../assets/twitter.svg';
+import YoutubeSVG from '../assets/youtube.svg';
+
+const social = [
+  {
+    id: 1,
+    icon: <FacebookSVG />,
+  },
+  {
+    id: 2,
+    icon: <InstagrammSVG />,
+  },
+  {
+    id: 3,
+    icon: <TwitterSVG />,
+  },
+  {
+    id: 4,
+    icon: <YoutubeSVG />,
+  },
+];
+
+export default social;
