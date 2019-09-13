d8b427db24899b4800f79d8e5a8f407965492175
OneGoodReas0n <korobovi4@gmail.com>
2019-09-13 19:06:14 +0300

    Implementing and setup Support page

---
+++ app/containers/Support/tests/saga.test.js
@@ -0,0 +1,15 @@
+/**
+ * Test sagas
+ */
+
+/* eslint-disable redux-saga/yield-effects */
+// import { take, call, put, select } from 'redux-saga/effects';
+// import supportSaga from '../saga';
+
+// const generator = supportSaga();
+
+describe('supportSaga Saga', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
